255ec98ab22e65ba46310bf09f5648813356bc58
Brenden94 <brendenletsatsi1@gmail.com>
2023-06-20 02:32:56 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,6 @@
-#include "unistd.h"
+#include <unistd.h>
+
+#define COMMA_SPACE putchar_errcheck(44), putchar_errcheck(32)
 
 /**
  * putchar_errcheck - Writes a character to the standard output
@@ -8,37 +10,48 @@
  */
 int putchar_errcheck(int c)
 {
-if (write(1, &c, 1) == -1)
-return (-1);
-return (1);
+    return (write(1, &c, 1) == -1 ? -1 : 1);
 }
 
 /**
- * print_combinations - Prints combinations of numbers
+ * print_combinations - Prints all possible combinations of three digits
+ *
+ * Description: This function prints all possible different combinations of
+ * three digits. The digits are separated by a comma and a space. Numbers
+ * should be printed in ascending order. Only five calls to putchar_errcheck
+ * are allowed. The program returns a SUCCESS value.
  */
 void print_combinations(void)
 {
-putchar_errcheck(0); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(1); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(2); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(3); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(4); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(5); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(6); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(7); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(8); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(0); putchar_errcheck(9); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(1); putchar_errcheck(2); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(1); putchar_errcheck(3); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(1); putchar_errcheck(4); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(1); putchar_errcheck(5); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(1); putchar_errcheck(6); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(1); putchar_errcheck(7); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(1); putchar_errcheck(8); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(1); putchar_errcheck(9); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(2); putchar_errcheck(3); putchar_errcheck(,); putchar_errcheck( );
-putchar_errcheck(2); putchar_errcheck(4); putchar_errcheck(,); putchar_errcheck( );
-/* Add more combinations here */
-putchar_errcheck(n);
+    int i, j, k;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+        {
+            for (k = j + 1; k <= 9; k++)
+            {
+                putchar_errcheck(i + 0);
+                putchar_errcheck(j + 0);
+                putchar_errcheck(k + 0);
+
+                if (i != 7 || j != 8 || k != 9)
+                    COMMA_SPACE;
+            }
+        }
+    }
+
+    putchar_errcheck(n);
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+    print_combinations();
+
+    return (0);
+}
