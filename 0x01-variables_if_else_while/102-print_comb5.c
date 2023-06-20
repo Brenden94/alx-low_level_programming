9f3e4e8151f1e70e400109c5363b904f50aec36f
Brenden94 <brendenletsatsi1@gmail.com>
2023-06-20 03:02:33 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,18 +17,18 @@ int main(void)
             {
                 putchar((i / 10) + '0');
                 putchar((i % 10) + '0');
-                putchar(' ');
+                putchar( );
                 putchar((j / 10) + '0');
                 putchar((j % 10) + '0');
                 if (i != 98 || j != 99)
                 {
                     putchar(,);
-                    putchar(' ');
+                    putchar( );
                 }
             }
         }
     }
 
-    putchar('\n');
+    putchar(n);
     return (0);
 }
