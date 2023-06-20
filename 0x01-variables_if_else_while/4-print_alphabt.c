8e1ba5c5815438bd396beffa85d8bef3c3645a48
Brenden94 <brendenletsatsi1@gmail.com>
2023-06-19 18:16:43 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the alphabet in lowercase,
+ * excluding the letters e and q.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     char ch = 'a';
