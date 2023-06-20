e15a27f1d5e799e53f597d0acddc66da6b36ce3c
Brenden94 <brendenletsatsi1@gmail.com>
2023-06-20 02:37:13 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+#define COMMA_SPACE putchar_errcheck(44); putchar_errcheck(32)
+
+/**
+ * putchar_errcheck - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, 1. On error, -1 is returned.
+ */
+int putchar_errcheck(int c)
+{
+    return (write(1, &c, 1) == -1 ? -1 : 1);
+}
+
+/**
+ * print_combinations - Prints all possible combinations of two two-digit numbers
+ *
+ * Description: This function prints all possible combinations of two two-digit
+ * numbers. The numbers range from 0 to 99 and are separated by a space. All
+ * numbers are printed with two digits. The combinations are separated by a
+ * comma and a space. The program uses only eight calls to putchar_errcheck.
+ * All code is contained within the main function.
+ */
+int main(void)
+{
+    int num1, num2;
+
+    for (num1 = 0; num1 <= 98; num1++)
+    {
+        for (num2 = num1 + 1; num2 <= 99; num2++)
+        {
+            putchar_errcheck((num1 / 10) + 0);
+            putchar_errcheck((num1 % 10) + 0);
+            putchar_errcheck(32);
+            putchar_errcheck((num2 / 10) + 0);
+            putchar_errcheck((num2 % 10) + 0);
+
+            if (!(num1 == 98 && num2 == 99))
+                COMMA_SPACE;
+        }
+    }
+
+    putchar_errcheck(10);
+
+    return (0);
+}
