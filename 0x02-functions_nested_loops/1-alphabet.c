f1f728f435b1ea9bed5aca2e3c04694708af1943
Brenden94 <brendenletsatsi1@gmail.com>
2023-09-12 05:13:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,11 @@
  */
 void print_alphabet(void)
 {
-char c = 'a';
-
-while (c <= 'z')
-{
-');
+    char c = a;
+    while (c <= z)
+    {
+        _putchar(c);
+        c++;
+    }
+    _putchar(n);
 }
