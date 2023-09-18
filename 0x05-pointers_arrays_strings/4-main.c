8a5ce36126cdde3d26951761b3398f0791025d57
Brenden94 <brendenletsatsi1@gmail.com>
2023-09-18 16:16:15 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int main(void)
+{
+    char *str1 = "Holberton!";
+    char *str2 = "Lorem ipsum...";
+    char *str3 = "";
+
+    _puts("str = \"Holberton!\";"); // regular string
+    _print_rev(str1);
+
+    _puts("str = \"Lorem ipsum...\";"); // long string
+    _print_rev(str2);
+
+    _puts("str = \"\";"); // empty string
+    _print_rev(str3);
+
+    return (0);
+}
+
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,15 +12,16 @@ void _print_rev(char *str)
 if (str != NULL)
 {
 int length = 0;
+int i;
 
 /* Calculate the length of the string */
-while (str[length] != 0)
+while (str[length] != '\0')
 {
 length++;
 }
 
 /* Print the string in reverse order */
-for (int i = length - 1; i >= 0; i--)
+for (i = length - 1; i >= 0; i--)
 {
 _putchar(str[i]);
 }
