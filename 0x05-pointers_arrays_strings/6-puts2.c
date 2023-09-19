ddd90e701087603bfd47d487ff9ec1cb9568d8a0
Brenden94 <brendenletsatsi1@gmail.com>
2023-09-19 01:54:04 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string
- * @str: The input string
+ * puts2 - Prints every other character of a string.
+ * @str: The string to print.
  */
 void puts2(char *str)
 {
@@ -11,7 +11,9 @@ int i = 0;
 while (str[i] != 0)
 {
 if (i % 2 == 0)
+{
 _putchar(str[i]);
+}
 i++;
 }
 _putchar(n);
