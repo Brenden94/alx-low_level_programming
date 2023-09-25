b4effacc953062613fd8d216101bfb2852826320
Brenden94 <brendenletsatsi1@gmail.com>
2023-09-25 13:38:23 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,13 +10,13 @@ char *cap_string(char *str)
 {
     char separators[] = " \t\n,;.!?\"(){}";
     int i, j;
-    int capitalize_next = 1; // Flag to check if the next word should be capitalized
+    int capitalize_next = 1; /* Flag to check if the next word should be capitalized */
 
     for (i = 0; str[i] != 0; i++)
     {
         if (capitalize_next && (str[i] >= a && str[i] <= z))
         {
-            str[i] -= 32; // Convert to uppercase
+            str[i] -= 32; /* Convert to uppercase */
             capitalize_next = 0;
         }
 
