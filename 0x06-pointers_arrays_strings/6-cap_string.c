c13059514837a22eff9618688d0699e2643f86c8
Brenden94 <brendenletsatsi1@gmail.com>
2023-09-25 13:35:07 -0700

    ./6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,40 +2,32 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- * @str: The string to capitalize.
+ * @str: The input string.
  *
  * Return: A pointer to the modified string.
  */
 char *cap_string(char *str)
 {
-    int i = 0;
+    char separators[] = " \t\n,;.!?\"(){}";
+    int i, j;
+    int capitalize_next = 1; // Flag to check if the next word should be capitalized
 
-    if (str[i] >= 'a' && str[i] <= 'z')
+    for (i = 0; str[i] != 0; i++)
     {
-        str[i] = str[i] - 32; /* Convert first letter to uppercase */
-    }
-    i++;
+        if (capitalize_next && (str[i] >= a && str[i] <= z))
+        {
+            str[i] -= 32; // Convert to uppercase
+            capitalize_next = 0;
+        }
 
-    while (str[i] != '\0')
-    {
-        char separators[] = " \t\n,;.!?\"(){}";
-        int is_separator = 0;
-
-        for (int j = 0; separators[j]; j++)
+        for (j = 0; separators[j] != 0; j++)
         {
             if (str[i] == separators[j])
             {
-                is_separator = 1;
+                capitalize_next = 1;
                 break;
             }
         }
-
-        if (is_separator && str[i + 1] >= 'a' && str[i + 1] <= 'z')
-        {
-            str[i + 1] = str[i + 1] - 32; /* Convert next letter to uppercase */
-        }
-
-        i++;
     }
 
     return str;
