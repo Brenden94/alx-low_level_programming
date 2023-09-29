4170dbb4d3595868af5e4f0a907d2fe03d054377
Brenden94 <brendenletsatsi1@gmail.com>
2023-09-29 04:14:47 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-
+/* Function prototype for _putchar */
 int _putchar(char c);
-void _puts_recursion(char *s);
+
+/* Function prototype for _print_rev_recursion */
+void _print_rev_recursion(char *s);
 
 #endif /* MAIN_H */
