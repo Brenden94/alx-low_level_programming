83dde41fa4d18cac8cc09f613a6999c569bb9091
Brenden94 <brendenletsatsi1@gmail.com>
2023-10-10 12:52:46 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,23 +5,40 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        printf("Error\\n");
+        printf("Error\n");
         return (98);
     }
 
     char *num1 = argv[1];
     char *num2 = argv[2];
 
-    /*
-     * Implement your multiplication logic here.
-     * You may need to use atoi or strtol to convert strings to integers.
-     * Be sure to handle invalid inputs as described in the task.
-     */
+    /* Check if num1 and num2 contain only digits */
+    for (int i = 0; num1[i] != '\\0'; i++)
+    {
+        if (num1[i] < '0' || num1[i] > '9')
+        {
+            printf("Error\n");
+            return (98);
+        }
+    }
 
-    /*
-     * Print the result followed by a newline.
-     */
-    printf("%d\\n", result);
+    for (int i = 0; num2[i] != '\\0'; i++)
+    {
+        if (num2[i] < '0' || num2[i] > '9')
+        {
+            printf("Error\n");
+            return (98);
+        }
+    }
+
+    /* Implement your multiplication logic here. */
+    /* Convert num1 and num2 to integers using atoi or strtol. */
+    /* Perform the multiplication and store the result in an integer variable. */
+
+    int result = atoi(num1) * atoi(num2);
+
+    /* Print the result followed by a newline. */
+    printf("%d\n", result);
 
     return (0);
 }
