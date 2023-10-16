b17e3f77d1ad56555647cde12a1ad0be892c5b8d
Brenden94 <brendenletsatsi1@gmail.com>
2023-10-16 02:23:06 -0700

    2-print_strings.c

--- 0x10-variadic_functions/main_0.c
+++ 0x10-variadic_functions/main_0.c
@@ -1,12 +1,7 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 
 int main(void)
 {
-    int sum;
-
-    sum = sum_them_all(2, 278634, 87365);
-    printf("%d\n", sum);
-
+    print_strings(", ", 4, "You", "know", "nothing", "Jon Snow");
     return (0);
 }
