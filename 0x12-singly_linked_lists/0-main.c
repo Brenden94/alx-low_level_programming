360efdf6cabcb69414714eca8e27d449e5d2506a
Brenden94 <brendenletsatsi1@gmail.com>
2023-10-19 01:14:01 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
@@ -17,23 +19,21 @@ int main(void)
     new = malloc(sizeof(list_t));
     if (new == NULL)
     {
-        printf("Error\\n");
+        printf("Error\n");
         return (1);
     }
     new->str = strdup("Hello");
     new->len = 5;
     new->next = head;
     head = new;
-
     n = print_list(head);
-    printf("-> %lu elements\\n", n);
+    printf("-> %lu elements\n", n);
 
     printf("\\n");
-
     free(new->str);
     new->str = NULL;
     n = print_list(head);
-    printf("-> %lu elements\\n", n);
+    printf("-> %lu elements\n", n);
 
     free(new);
     return (0);
--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,30 @@
-```c
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * print_list - Prints all the elements of a list_t list.
- * @h: A pointer to the list.
- *
+ * @h: A pointer to the list_t list.
  * Return: The number of nodes.
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+    size_t node_count = 0;
 
-    while (h != NULL)
+    while (h)
     {
-        if (h->str == NULL)
-            printf("[0] (nil)\\n");
+        if (h->str)
+        {
+            printf("[%d] %s\\n", h->len, h->str);
+        }
         else
-            printf("[%u] %s\\n", h->len, h->str);
+        {
+            printf("[0] (nil)\\n");
+        }
 
-        count++;
+        node_count++;
         h = h->next;
     }
 
-    return count;
+    return (node_count);
 }
-```
