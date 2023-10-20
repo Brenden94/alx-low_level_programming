605e1a15443083b08a64621725319e3f912c97cc
Brenden94 <brendenletsatsi1@gmail.com>
2023-10-20 11:22:33 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,15 @@
 #include "main.h"
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ *
+ * Return: On success 1, on error -1, and errno is set appropriately
+ */
+int _putchar(char c);
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s: The string to be reversed and printed
+ * _print_rev_recursion - Prints a string in reverse using recursion
+ * @s: The string to print
  */
 void _print_rev_recursion(char *s)
 {
