1786635f5d3bbfa8e0d37cc3f503c20cf75646a4
Brenden94 <brendenletsatsi1@gmail.com>
2023-10-24 06:01:13 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s number_of_bytes\n", argv[0]);
-        return (1);
-    }
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s number_of_bytes\n", argv[0]);
+		return 1;
+	}
 
-    int num_bytes = atoi(argv[1]);
-    if (num_bytes <= 0) {
-        printf("Error\n");
-        return (2);
-    }
+	int num_bytes = atoi(argv[1]);
 
-    for (int i = 0; i < num_bytes; i++) {
-        printf("%02hhx", *((char *)main + i));
-        if (i < num_bytes - 1)
-            printf(" ");
-        else
-            printf("\n");
-    }
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return 2;
+	}
 
-    return (0);
+	unsigned char *main_address = (unsigned char *)main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_address[i]);
+		if (i < num_bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return 0;
 }
