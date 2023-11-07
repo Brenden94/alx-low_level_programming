37c54b71e73d1c869649d83c92b08c2f76da0abf
Brenden94 <brendenletsatsi1@gmail.com>
2023-11-07 01:36:23 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include "main.h"
 
-/**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed, or 0 on error.
- */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-		return 0;
+    ssize_t read_bytes;
+    char *buffer;
+    FILE *file;
 
-	FILE *file = fopen(filename, "r");
-	if (file == NULL)
-		return 0;
+    if (filename == NULL)
+        return (0);
 
-	char *buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		fclose(file);
-		return 0;
-	}
+    file = fopen(filename, "r");
+    if (file == NULL)
+        return (0);
 
-	ssize_t read_bytes = fread(buffer, 1, letters, file);
-	ssize_t write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+    buffer = malloc(letters);
+    if (buffer == NULL)
+    {
+        fclose(file);
+        return (0);
+    }
 
-	free(buffer);
-	fclose(file);
+    read_bytes = fread(buffer, 1, letters, file);
+    if (read_bytes == -1)
+    {
+        free(buffer);
+        fclose(file);
+        return (0);
+    }
 
-	if (read_bytes == write_bytes)
-		return read_bytes;
-	else
-		return 0;
+    write(STDOUT_FILENO, buffer, read_bytes);
+
+    free(buffer);
+    fclose(file);
+    return (read_bytes);
 }
