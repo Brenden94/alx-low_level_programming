c4fe58e2fad116124fe36d11f8be4f798178058f
Brenden94 <brendenletsatsi1@gmail.com>
2023-12-01 12:32:01 -0800

    0x10-variadic_functions

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * main - Check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_strings(", ", 2, "Jay", "Django");
+    print_numbers(", ", 3, 0, 98, -1024);
     return (0);
 }
+
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,46 +1,47 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    unsigned int i = 0;
-    char *str;
-    const char *separator = "";
+	va_list args;
+	unsigned int i = 0;
+	char *str;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-        case 'c':
-            printf("%s%c", separator, va_arg(args, int));
-            break;
-        case 'i':
-            printf("%s%d", separator, va_arg(args, int));
-            break;
-        case 'f':
-            printf("%s%f", separator, va_arg(args, double));
-            break;
-        case 's':
-            str = va_arg(args, char *);
-            if (!str)
-                str = "(nil)";
-            printf("%s%s", separator, str);
-            break;
-        default:
-            separator = "";
-            i++;
-            continue;
-        }
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (format[i + 1])
+			printf(", ");
+		i++;
+	}
+	printf("\n");
 
-        separator = ", ";
-        i++;
-    }
-
-    printf("\n");
-
-    va_end(args);
+	va_end(args);
 }
-
